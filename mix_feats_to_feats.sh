e83e3c0a57ed0e7288239abe1fd47416c8ef74a6
billy <yuchenshiwin@gmail.com>
2021-01-14 21:44:14 +0100

    delete not used files

--- mix_feats_to_feats.sh
+++
@@ -1,4 +0,0 @@
-path='/usr/home/shi/projects/data_aishell/data/mix_train_clean_match'
-pathdev='/usr/home/shi/projects/data_aishell/data/mix_dev_clean_match'
-sed 's/__mix0//g' ${path}/mix_feats.scp >${path}/feats.scp
-sed 's/__mix0//g' ${pathdev}/mix_feats.scp >${pathdev}/feats.scp
